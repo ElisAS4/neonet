db3f39af54657fe21d31ad3d603f75afa4396abe
ElisAS4 <ayravolpato@gmail.com>
2025-11-23 09:44:16 -0300

    Update webpack.config.js

--- neonet_v2.1_project_fixed/neonet_v2.1_project_working/neonet/neonet/clients/web/webpack.config.js
+++ neonet_v2.1_project_fixed/neonet_v2.1_project_working/neonet/neonet/clients/web/webpack.config.js
@@ -1,45 +1,59 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js", // Entry point for your main application logic
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // Clean the output directory before building
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
+      template: "./index_scalable.html", // Use your new main HTML file as a template
+      filename: "index_scalable.html", // Output file name in dist
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "mock-dapps/neonet-chat",
+          to: "mock-dapps/neonet-chat",
+        },
+        {
+          from: "mock-dapps/neonet-videos",
+          to: "mock-dapps/neonet-videos",
+        },
+        // REMOVIDO: { from: "src/utils/PeerManager_scalable.js", to: "src/utils/PeerManager_scalable.js" },
+      ],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Ensure both .js and .jsx files are processed by Babel
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
     port: 3000,
-    hot: true,
   },
-  resolve: {
-    extensions: ['.js', '.json'],
-  },
-  mode: 'development',
 };
-
